462ce627b9e836da1935e7b760e5b70cea6c280d
Thiago Souza <thisimaso@gmail.com>
2016-10-28 21:40:50 -0200

    Adicionando o bootstrap diretamente pela url na index e criacao de um estilo simples para complementar a index. Index criada

--- index.php
+++ index.php
@@ -3,9 +3,18 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Menu</title>
+		<link rel="stylesheet" type="text/css" href="estilo/default.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	</head>
 	<body>
-		<a href="visao/professor/index-prof.php">Professores</a><br>
-		<a href="visao/aluno/index-aluno.php">Alunos</a>
+			<h1>Sistema Escolas</h1>
+		<section>
+		<div class="mid">
+			<ul class="nav nav-pills nav-stacked">
+				<li><a href="visao/professor/index-prof.php" >Professores</a></li>
+				<li><a href="visao/aluno/index-aluno.php" >Alunos</a></li>
+			</ul>
+		</div>
+		</section>
 	</body>
 </html>
