78c0bde15755656632bc16709ed815850c989566
Thiago Souza <thisimaso@gmail.com>
2016-10-29 16:52:56 -0200

    Layout inicial de todas as paginas criadas

--- visao/professor/alterar-prof.php
+++ visao/professor/alterar-prof.php
@@ -7,27 +7,30 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	</head>
 	<body>
-		<form method="post" action="../dao/alunoDao">
-		 <div class="form-group">
-		    <label for="exampleInputEmail1">Nome</label>
-		    <input type="text" class="form-control" name="nome" placeholder="Aqui vem o nome">
-		  </div>
-		  <div class="form-group">
-		    <label for="exampleInputEmail1">Data Admissão</label>
-		    <input type="date" class="form-control" name="dataAd">
-		  </div>
-		  <div class="form-group">
-		    <label for="exampleInputPassword1">Disciplina</label>
-		    <select name="disciplina">
-				<option value="1">Multimidia</option>
-				<option value="2">Analise e desenvolvimento de sistemas</option>
-				<option value="3">Sistemas para internet</option>
-				<option value="4">Redes</option>
-				<option value="5">Publicidade</option>
-				<option value="6">Jornalismo</option>
-			</select>
-		  </div>
-		  <button type="submit" class="btn btn-default">Enviar</button>
-		</form>
+		<div class="mid aumentar">
+			<form method="post" action="../dao/alunoDao" class="formu">
+			 <div class="form-group">
+			    <label for="exampleInputEmail1">Nome</label>
+			    <input type="text" class="form-control" name="nome" placeholder="Aqui vem o nome">
+			  </div>
+			  <div class="form-group">
+			    <label for="exampleInputEmail1">Data Admissão</label>
+			    <input type="date" class="form-control" name="dataAd">
+			  </div>
+			  <div class="form-group">
+			    <label for="exampleInputPassword1">Disciplina</label>
+			    <select name="disciplina">
+					<option value="1">Multimidia</option>
+					<option value="2">Analise e desenvolvimento de sistemas</option>
+					<option value="3">Sistemas para internet</option>
+					<option value="4">Redes</option>
+					<option value="5">Publicidade</option>
+					<option value="6">Jornalismo</option>
+				</select>
+			  </div>
+			  <button type="submit" class="btn btn-default">Enviar</button>
+			</form>
+		</div>
+		
 	</body>
 </html>
