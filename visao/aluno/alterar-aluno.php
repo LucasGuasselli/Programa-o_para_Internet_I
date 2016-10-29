e62cea3bbca208185e3e6af47b7b547b5928d56b
Thiago Souza <thisimaso@gmail.com>
2016-10-28 22:34:13 -0200

    Html inicial de alunos prontos para teste, porem incompletos

---
+++ visao/aluno/alterar-aluno.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Menu</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	</head>
+	<body>
+		<form method="post" action="../dao/alunoDao">
+		 <div class="form-group">
+		    <label for="exampleInputEmail1">Nome</label>
+		    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Aqui vem o nome">
+		  </div>
+		  <div class="form-group">
+		    <label for="exampleInputEmail1">Email</label>
+		    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+		  </div>
+		  <div class="form-group">
+		    <label for="exampleInputPassword1">Curso</label>
+		    <select name="curso">
+				<option value="1">Multimidia</option>
+				<option value="2">Analise e desenvolvimento de sistemas</option>
+				<option value="3">Sistemas para internet</option>
+				<option value="4">Redes</option>
+				<option value="5">Publicidade</option>
+				<option value="6">Jornalismo</option>
+			</select>
+		  </div>
+		  <button type="submit" class="btn btn-default">Submit</button>
+		</form>
+	</body>
+</html>
--- visao/aluno/index-aluno.php
+++ visao/aluno/index-aluno.php
@@ -2,9 +2,31 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title></title>
+		<title>Alunos</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	</head>
 	<body>
-		
+		<form method="post" action="../dao/alunoDao" class="form-inline">
+		  <div class="form-group">
+		    <label for="exampleInputName2">Nome</label>
+		    <input type="text" class="form-control" name="nome" placeholder="Jane Doe">
+		  </div>
+		  <div class="form-group">
+		    <label for="exampleInputName2">E-mail</label>
+		    <input type="email" class="form-control" name="email" placeholder="Exemplo@hotmail.com">
+		  </div>
+		  <div class="form-group">
+		    <label for="exampleInputName2">Curso</label>
+		    <select name="curso">
+				<option value="1">Multimidia</option>
+				<option value="2">Analise e desenvolvimento de sistemas</option>
+				<option value="3">Sistemas para internet</option>
+				<option value="4">Redes</option>
+				<option value="5">Publicidade</option>
+				<option value="6">Jornalismo</option>
+			</select>
+		  </div>
+		  <button type="submit" class="btn btn-default">Submit</button>	
+		</form>
 	</body>
 </html>
